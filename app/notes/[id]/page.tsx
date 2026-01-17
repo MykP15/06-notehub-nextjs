ae031937e95c59ce49c0ee7ef7c148117e0bfa82
Mykhailo Pyroh <mykhailopk@gmail.com>
2026-01-17 21:46:29 +0100

    first try

---
+++ app/notes/[id]/page.tsx
@@ -0,0 +1,26 @@
+import { fetchNoteById } from "@/lib/api"
+import { QueryClient } from "@tanstack/react-query"
+import NoteDetails from "../NoteDetails.client"
+
+interface NoteProps{
+    params: Promise<{ id: string }>
+}
+
+
+async function Note({ params }: NoteProps) {
+
+    const {id} = await params
+
+    const queryClient = new QueryClient()
+
+    queryClient.prefetchQuery({
+        queryKey: ["note", id],
+        queryFn: () => fetchNoteById(id)
+})
+
+    return (
+       <NoteDetails />
+    )
+}
+
+export default Note
